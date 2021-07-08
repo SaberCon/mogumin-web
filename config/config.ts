12a8f165b167e6e364deacd8dfe7e98a244fc7b7
SaberCon <894876785@qq.com>
2021-07-08 23:42:28 +0800

    :art: update config

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,11 @@
 // https://umijs.org/config/
 import { defineConfig } from 'umi';
-import { join } from 'path';
 import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 import routes from './routes';
+
 const { REACT_APP_ENV } = process.env;
+
 export default defineConfig({
   hash: true,
   antd: {},
@@ -43,15 +44,9 @@ export default defineConfig({
   openAPI: [
     {
       requestLibPath: "import { request } from 'umi'",
-      // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-      schemaPath: join(__dirname, 'oneapi.json'),
-      mock: false,
-    },
-    {
-      requestLibPath: "import { request } from 'umi'",
-      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+      schemaPath: 'http://localhost:8080/v3/api-docs',
       projectName: 'swagger',
+      mock: false,
     },
   ],
   nodeModulesTransform: {
